dda38a58d083305c6f60fcf48161658cdacafecb
butlerajamesab-lab <butlerajames.ab@gmail.com>
2026-08-09 09:08:53 -0700

    Add bounded source-artifact custody rollback

---
+++ supabase/rollback/20260809190000_kaleidoscope_source_artifact_custody_rollback.sql
@@ -0,0 +1,7 @@
+begin;
+
+drop table if exists kaleidoscope.state_component_artifact;
+drop table if exists kaleidoscope.state_snapshot_artifact;
+drop table if exists kaleidoscope.source_artifact;
+
+commit;
